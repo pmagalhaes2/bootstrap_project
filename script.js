b335f740890e4370a6290d0e6847685046219985
Patricia Magalhães <paath.magalhaes2@gmail.com>
2021-08-25 21:46:19 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+    $("#loginAluno").submit(function(event) {
+
+        var usuario = $("inputUsuario").val()
+        var senha = $("inputSenha").val()
+
+        // Testar se usuario e senha estão corretos
+        if (usuario == "root" && senha == "1234") {
+            //add classes CSS (alert) na DIV
+            $("#mensagem").addClass("alert alert-success alert-dismissible fade show")
+
+            //remove a class alert-warning caso ela exista
+            $("#mensagem").removeClass("alert-warning")
+
+            //exibe a mensagem para o usuario dentro do alert
+            $("#mensagem").text("Usuário logado!").show().fadeOut(3000)
+
+        } else {
+
+            //add classes CSS (alert) na DIV
+            $("#mensagem").addClass("alert alert-warning alert-dismissible fade show")
+
+            //remove a class alert-success caso ela exista
+            $("#mensagem").removeClass("alert-success")
+
+            //exibe a mensagem para o usuario dentro do alert
+            $("#mensagem").text("Usuário ou senha inválida!").show().fadeOut(3000)
+            event.preventDefault() //bloqueia o evento de submit
+        }
+    })
+})
